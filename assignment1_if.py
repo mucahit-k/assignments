4ae921d3e25323db375ea3520b5497550360b634
mucahit <mucahitpa@gmail.com>
2020-04-15 11:49:42 -0400

    assignment1_if.py is added

---
+++ assignment1_if.py
@@ -0,0 +1,5 @@
+first_name = input('Enter your first name: ').lower()
+if first_name == 'mucahit':
+    print('Hello, Mucahit! The password is 1234')
+else:
+    print(f'Hello, {first_name.capitalize()}! See you later.')
