59db7c4f0503cda65bd5893a5aa2520e4111c2b5
mucahit <mucahitpa@gmail.com>
2020-04-24 17:57:22 -0400

    new

--- assignment2_if.py
+++ assignment2_if.py
@@ -13,7 +13,3 @@ else:
         else:
             print('You are not at risk of death from coronavirus, even though you need to be careful about it.')
 
-name = input('enter the name: ')
-text = f"Hello {name},\n Thank you for purchasing Herbalife products. I hope you are happy with Herbalife products.\n For your future Herbalife product purchases, I recommend you to use this site. It is Herbalife's site for me, as a distributor.\n You can buy there with %25 off with the code 25OFF25 \n You will just enter your email and create a password, and you are ready to go. You can use Paypal.
-\n\n https://koca.goherbalife.com/en-us \n\n Thank you \n Koca"
-print(text)
