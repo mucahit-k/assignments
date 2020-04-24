8c9f92d9be68fd1a2e60975504c518fdf21daef1
mucahit <mucahitpa@gmail.com>
2020-04-24 17:40:28 -0400

    assignment 3 updated

--- assignment3_if.py
+++ assignment3_if.py
@@ -1,15 +1,13 @@
-num_1 = input('Enter a number to check if it is an Armstrong number: ')
-
-if ',' in num_1 or '.' in num_1:
-    print('Please enter an integer number')
-elif num_1[0] == '-':
-    print('Please enter a positive number')
-elif not num_1.isdigit():
-    print('Do not use any entries other than numeric values')
-else:
-    num_2 = list(num_1)
-    num_list = [int(i)**len(num_2) for i in num_2]
-    if sum(num_list) == int(num_1):
-        print(f'{num_1} is an Armstrong number')
+num_1 = input('Enter a positive number to check if it is an Armstrong number: ')
+while True:
+    
+    if not num_1.isdigit():
+        num_1 = input('Enter a positive number to check if it is an Armstrong number: ')
     else:
-        print(f'{num_1} is not an Armstrong number')
+        num_2 = list(num_1)
+        num_list = [int(i)**len(num_2) for i in num_2]
+        if sum(num_list) == int(num_1):
+            print(f'{num_1} is an Armstrong number')
+        else:
+            print(f'{num_1} is not an Armstrong number')
+        break
