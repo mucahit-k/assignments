551a4737f5c58fc698f24bce178350d0842d70df
mucahit <mucahitpa@gmail.com>
2020-05-30 17:49:12 -0400

    api

---
+++ api.py
@@ -0,0 +1,13 @@
+from flask import Flask, json
+
+
+students = [{"id": 1, "name": "John"}, {"id": 2, "name": "Mucahit"}]
+
+api = Flask(__name__)
+
+@api.route('/', methods=['GET'])
+def get_students():
+    return json.dumps(students)
+
+if __name__ == '__main__':
+    api.run(host='0.0.0.0', port=80)
