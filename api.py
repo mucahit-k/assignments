908992ce39cac9e590510fb9c9eb6c48ca2af9be
mucahit <mucahitpa@gmail.com>
2020-06-02 17:21:06 -0400

    api

--- api.py
+++ api.py
@@ -1,13 +1,22 @@
-from flask import Flask, json
+from flask import Flask, json, request
 
-
-students = [{"id": 1, "name": "John"}, {"id": 2, "name": "Mucahit"}]
 
 api = Flask(__name__)
 
-@api.route('/', methods=['GET'])
+def converter(num):
+    roman_nums = {1000: 'M', 900: 'CM', 500: 'D', 400: 'CD', 100: 'C', 90: 'XC', 50: 'L', 40: 'XL', 10: 'X', 9: 'IX', 5: 'V', 4: 'IV', 1: 'I'}
+    rom = []
+    for i in roman_nums:
+        k = num // i
+        num -= k * i
+        n = k*roman_nums[i]
+        rom.append(n)
+    return ''.join(rom)
+
+@api.route('/', methods=['POST'])
 def get_students():
-    return json.dumps(students)
+    alpha = request.form["number"]
+    return converter(alpha)
 
 if __name__ == '__main__':
     api.run(host='0.0.0.0', port=80)
